b0b3e083a6a9a4567fadb42cb008ab0fc788aca0
halbykevin <halbykevin@hotmail.com>
2024-05-01 11:36:17 +0300

    testing

--- game_4/linking_game4.js
+++ game_4/linking_game4.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   var startButton = document.getElementById("startButton");
   startButton.addEventListener("click", function () {
-    window.location.href = "setup_game3.html"; // Redirect to the setup page
+    window.location.href = "setup_game4.html"; // Redirect to the setup page
   });
 });
