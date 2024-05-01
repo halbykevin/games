be1ed0ccc0c25966448d4b930b67d0941adf0940
halbykevin <halbykevin@hotmail.com>
2024-05-02 00:44:42 +0300

    game 5 setup complete but withput design

---
+++ game_5/linking_game5.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var startButton = document.getElementById("startButton");
+  startButton.addEventListener("click", function () {
+    window.location.href = "setup_game5.html"; // Redirect to the setup page
+  });
+});
